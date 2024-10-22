b62bf182e8bdb8a2f2c7fd8aa863286d9b04d10e
Carlos Gabriel <148346104+CarlosGabrielRocha@users.noreply.github.com>
2024-10-22 15:52:06 -0300

    ex25

---
+++ modulo-5/15-ex25/index.js
@@ -0,0 +1,34 @@
+async function calcImc(weight, height) {
+    if (typeof weight === 'number' && typeof height === 'number') {
+        return (weight / (height * height)).toFixed(2)
+    } else {
+        return Promise.reject('Os parâmetros precisam ser do tipo number!')
+    }
+}
+
+async function showImc(weight, height) {
+
+    try {
+        const result = await calcImc(weight, height)
+        console.log('Calculando IMC..')   
+        console.log(`Resultado: ${result}`)
+        if (result < 18.5) {
+            console.log('Situação: Magreza')
+        } else if (result >= 18.5 && result <= 24.9) {
+            console.log('Situação: Normal')
+        } else if (result >= 25 && result <= 29.9) {
+            console.log('Situação: sobrepeso')
+        } else if (result >= 30 && result <= 39.9) {
+            console.log('Situação: Obesidade')
+        } else {
+            console.log('Situação: Obesidade Grave')
+        }
+    } catch (err) {
+        console.log(`Resultado: ${err}`)
+    }
+
+}
+
+showImc(54, 1.65)
+showImc(54, 'gabriel')
+showImc(200, 1.75)
