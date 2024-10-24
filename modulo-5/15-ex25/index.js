dc42505f408c4933b97144354e3431a54d28d9c6
Carlos Gabriel <148346104+CarlosGabrielRocha@users.noreply.github.com>
2024-10-24 09:08:18 -0300

    ex25

--- modulo-5/15-ex25/index.js
+++ modulo-5/15-ex25/index.js
@@ -10,15 +10,15 @@ async function showImc(weight, height) {
 
     try {
         const result = await calcImc(weight, height)
-        console.log('Calculando IMC..')   
+        console.log('Calculando IMC..')
         console.log(`Resultado: ${result}`)
         if (result < 18.5) {
             console.log('Situação: Magreza')
-        } else if (result >= 18.5 && result <= 24.9) {
+        } else if (result <= 24.9) {
             console.log('Situação: Normal')
-        } else if (result >= 25 && result <= 29.9) {
+        } else if (result <= 29.9) {
             console.log('Situação: sobrepeso')
-        } else if (result >= 30 && result <= 39.9) {
+        } else if (result <= 39.9) {
             console.log('Situação: Obesidade')
         } else {
             console.log('Situação: Obesidade Grave')
@@ -32,3 +32,7 @@ async function showImc(weight, height) {
 showImc(54, 1.65)
 showImc(54, 'gabriel')
 showImc(200, 1.75)
+
+
+
+
